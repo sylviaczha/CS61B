2b0bf90163b8fec9d539a27b867e1a6526fe0684
Sylvia Zhang <czha7533@gmail.com>
2023-01-31 14:19:41 +0800

    Modify lab2

--- Assignments/lab2/IntList/IntListExercises.java
+++ Assignments/lab2/IntList/IntListExercises.java
@@ -14,6 +14,7 @@ public class IntListExercises {
             head.first += c;
             head = head.rest;
         }
+        head.first += c;
     }
 
     /**
@@ -26,7 +27,9 @@ public class IntListExercises {
     public static void setToZeroIfMaxFEL(IntList L) {
         IntList p = L;
         while (p != null) {
-            if (firstDigitEqualsLastDigit(max(p))) {
+            int currentMax = max(p);
+            boolean firstEqualsLast = firstDigitEqualsLastDigit(currentMax);
+            if (firstEqualsLast) {
                 p.first = 0;
             }
             p = p.rest;
@@ -51,7 +54,7 @@ public class IntListExercises {
      */
     public static boolean firstDigitEqualsLastDigit(int x) {
         int lastDigit = x % 10;
-        while (x > 10) {
+        while (x >= 10) {
             x = x / 10;
         }
         int firstDigit = x % 10;
@@ -69,14 +72,16 @@ public class IntListExercises {
         // Base Case: we have reached the end of the list
         if (lst == null) {
             return false;
+        } else {
+            /* If the current element is prime, then currElemIsPrime is true, and the current prime element is squared.
+             * and then proceed to the next element until all the elements are checked.*/
+            boolean currElemIsPrime = Primes.isPrime(lst.first);
+            if (currElemIsPrime) {
+                lst.first *= lst.first;
+                lst = lst.rest;
+                squarePrimes(lst);
+            }
+            return true;
         }
-
-        boolean currElemIsPrime = Primes.isPrime(lst.first);
-
-        if (currElemIsPrime) {
-            lst.first *= lst.first;
-        }
-
-        return currElemIsPrime || squarePrimes(lst.rest);
     }
 }
